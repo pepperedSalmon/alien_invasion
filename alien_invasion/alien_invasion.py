19e35b22f33a6f077b20485a8c9fc00fa99e444c
Ivan Lopez <[Ivanlg428@gmail.com]>
2020-07-10 01:49:36 -0700

    Refactor update & draw and add move

--- alien_invasion/alien_invasion.py
+++ alien_invasion/alien_invasion.py
@@ -23,18 +23,29 @@ class AlienInvasion:
     def run_game(self):
         """Start the main loop for the game."""
         while True:
-            # Watch for keyboard and mouse events.
-            for event in pygame.event.get():
-                if event.type == pygame.QUIT:
-                    sys.exit()
+            self._check_events()
+            self._update_screen()
+            
+    def _check_events(self):
+        """ Respond to keypresses and mouse events"""
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                sys.exit()
+            elif event.type==pygame.KEYDOWN:
+                if event.key==pygame.K_RIGHT:
+                    #Move the Ship to the right
+                    self.ship.rect.x+=1
+                elif event.key==pygame.K_LEFT:
+                    #Move The ship to the left
+                    self.ship.rect.x+=-1
 
-            # Redraw the screen during each pass through the loop.
-            self.screen.fill(self.settings.bg_color)
-
-            self.ship.blitme()
-            # Make the most recently drawn screen visible.
-            pygame.display.flip()
-
+    def _update_screen(self):
+        # Redraw the screen during each pass through the loop.
+        self.screen.fill(self.settings.bg_color)
+        # Redraw the Ship during each pass through the loop.
+        self.ship.blitme()
+        # Make the most recently drawn screen visible.
+        pygame.display.flip()
 if __name__ == '__main__':
     # Make a game instance, and run the game.
     ai = AlienInvasion()
--- alien_invasion/ship.py
+++ alien_invasion/ship.py
@@ -8,7 +8,7 @@ class Ship(object):
         self.screen_rect = ai_game.screen.get_rect()
 
         # Load the ship image and get its rect.
-        self.image = pygame.image.load('images/ship.bmp')
+        self.image = pygame.image.load('images/ship_blue.bmp')
         self.rect = self.image.get_rect()
 
         # Start each new ship at the bottom center of the screen.
