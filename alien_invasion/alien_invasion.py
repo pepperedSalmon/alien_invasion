0093f8734d83b1e198ac5cb56b0abf01df2c4b2f
Ivan Lopez <[Ivanlg428@gmail.com]>
2020-07-15 07:28:29 -0700

    Alien fleet motion

--- alien_invasion/alien_invasion.py
+++ alien_invasion/alien_invasion.py
@@ -34,6 +34,7 @@ class AlienInvasion:
             self._check_events()
             self.ship.update()
             self._update_bullets()
+            self._update_aliens()
             self._update_screen()
    
     def _check_events(self):
@@ -83,6 +84,14 @@ class AlienInvasion:
         for bullet in self.bullets.copy():
             if bullet.rect.bottom<=0:
                 self.bullets.remove(bullet)
+    def _update_aliens(self):
+        """
+        check if the fleet is at the edge,
+        then update the position of all the aliensin the fleet.
+        """
+        self._check_fleet_edges()
+        self.aliens.update()
+
     def _create_fleet(self):
         """Create the fleet of aliens."""
         # Create an alien and find the number of aliens in a row.
@@ -109,7 +118,18 @@ class AlienInvasion:
         alien.rect.x=alien.x
         alien.rect.y=alien.rect.height+2*alien.rect.height*row_number
         self.aliens.add(alien)
-
+    def _check_fleet_edges(self):
+        """ Respond appropriately if any aliens have reached an edge."""
+        for alien in self.aliens.sprites():
+            if alien.check_edges():
+                self._change_fleet_direction()
+                break
+    def _change_fleet_direction(self):
+        """Drop the entire fleet and change the fleet's direction."""
+        for alien in self.aliens.sprites():
+            alien.rect.y+=self.settings.fleet_drop_speed
+        self.settings.fleet_direction*=-1
+        
     def _update_screen(self):
         # Redraw the screen during each pass through the loop.
         self.screen.fill(self.settings.bg_color)
--- alien_invasion/settings.py
+++ alien_invasion/settings.py
@@ -3,15 +3,20 @@ class Settings:
 
     def __init__(self):
         """Initialize the game's settings."""
-        # Screen settings
+    # Screen settings
         self.screen_width = 1200
         self.screen_height = 800
         self.bg_color = (135, 206, 235)
-        #Ship settings
+    # Ship settings
         self.ship_speed=1.5
-        # Bullet settings
+    # Bullet settings
         self.bullet_speed=1.0
         self.bullet_width=3
         self.bullet_height=15
         self.bullet_color=(60,60,60)
         self.bullets_allowed=3
+    # Alien settings
+        self.alien_speed=1.0
+        self.fleet_drop_speed=10
+        #fleet_direction of 1 represents right; -1 represents left.
+        self.fleet_direction=1
