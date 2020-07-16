48e423fc5b20f4893ef9281a1f3dc1fbe7ecc47d
Ivan Lopez <[Ivanlg428@gmail.com]>
2020-07-15 18:48:51 -0700

    blue aliens & lives revise

--- alien_invasion/alien.py
+++ alien_invasion/alien.py
@@ -8,7 +8,7 @@ class Alien(Sprite):
         self.screen = ai_game.screen
         self.settings=ai_game.settings
         # Load the alien image and set its rect attribute.
-        self.image = pygame.image.load('images/alien.bmp')
+        self.image = pygame.image.load('images/alien_blue.bmp')
         self.rect = self.image.get_rect()
 
         # Start each new alien near the top left of the screen.
